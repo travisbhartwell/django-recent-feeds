500b77eb5d351f444a2be04e91d01a233334840d
Travis B. Hartwell <nafai@travishartwell.net>
2010-10-30 20:02:05 -0600

    Initial models and migrations.

--- recentfeeds/gourmetfeeds/models.py
+++ recentfeeds/gourmetfeeds/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Feed(models.Model):
+    user = models.ForeignKey(User)
+    source = models.URLField()
+
+class SeenFeedEntry(models.Model):
+    guid = models.CharField(max_length=32)
+    user = models.ForeignKey(User)
+
+
