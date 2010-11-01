c2c51492bfca7ab371a01a9a33f937139bf008ca
Travis B. Hartwell <nafai@travishartwell.net>
2010-11-01 02:49:37 -0600

    Added a very basic base template and displaying the logged in user name.

--- recentfeeds/gourmetfeeds/views.py
+++ recentfeeds/gourmetfeeds/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def welcome(request):
     if not request.user.is_authenticated():
         return HttpResponseRedirect('/gourmetfeeds/login/?next=%s' % request.path)
     else:
-        return render_to_response('gourmetfeeds/welcome.html')
+        return render_to_response('gourmetfeeds/welcome.html',
+                                  {},
+                                  context_instance=RequestContext(request))
