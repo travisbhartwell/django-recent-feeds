ed753f4f77c4bccea9712e1058c2bcfaf379bf89
Travis B. Hartwell <nafai@travishartwell.net>
2010-11-01 22:28:45 -0600

    Make South happy for migrations and check in migration script.

--- recentfeeds/gourmetfeeds/models.py
+++ recentfeeds/gourmetfeeds/models.py
@@ -1,12 +1,16 @@
-from django.db import models
+from datetime import datetime
+
 from django.contrib.auth.models import User
+from django.db import models
+
 
 class Feed(models.Model):
     user = models.ForeignKey(User)
     source = models.URLField()
-    subscribed_date = models.DateTimeField(auto_now_add = True)
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=256)
+    subscribed_date = models.DateTimeField(auto_now_add=True,
+                                           default=datetime.now)
+    title = models.CharField(max_length=50, default='')
+    description = models.CharField(max_length=256, default='')
 
     def __unicode__(self):
         return "[Feed %s for %s]" % (self.title, self.user)
@@ -15,7 +19,8 @@ class Feed(models.Model):
 class SeenFeedEntry(models.Model):
     guid = models.CharField(max_length=32)
     user = models.ForeignKey(User)
-    first_seen = models.DateTimeField(auto_now_add=True)
+    first_seen = models.DateTimeField(auto_now_add=True,
+                                      default=datetime.now)
 
     def __unicode__(self):
         return "[Entry %s for %s]" % (self.guid, self.user)
