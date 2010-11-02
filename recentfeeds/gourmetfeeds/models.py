9ce4a891df58147dfa50965327a6b6101d79eb2b
Travis B. Hartwell <nafai@travishartwell.net>
2010-11-01 22:10:29 -0600

    Add a bit more information to the models I'll eventually use.

--- recentfeeds/gourmetfeeds/models.py
+++ recentfeeds/gourmetfeeds/models.py
@@ -4,13 +4,18 @@ from django.contrib.auth.models import User
 class Feed(models.Model):
     user = models.ForeignKey(User)
     source = models.URLField()
+    subscribed_date = models.DateTimeField(auto_now_add = True)
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=256)
 
     def __unicode__(self):
-        return "[Feed %s for %s]" % (self.source, self.user)
+        return "[Feed %s for %s]" % (self.title, self.user)
+
 
 class SeenFeedEntry(models.Model):
     guid = models.CharField(max_length=32)
     user = models.ForeignKey(User)
+    first_seen = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return "[Entry %s for %s]" % (self.guid, self.user)
